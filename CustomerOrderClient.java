fadc71dd24a28b136f629f6235d9f7c404039719
VanceKingSaxbeA <vsaxbe@yahoo.com>
2014-02-14 04:34:55 +0530

    s

---
+++ CustomerOrderClient.java
@@ -0,0 +1,34 @@
+package com.GoldSax.EJB3.03.client;
+
+import com.GoldSax.EJB3.03.Address;
+import com.GoldSax.EJB3.03.CustOrderMgr;
+import com.GoldSax.EJB3.03.Customer;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class CustomerOrderClient
+{
+  public static void main(String[] args) {
+    try {
+      final Context context = new InitialContext();
+      CustOrderMgr custOrderMgr = 
+        (CustOrderMgr)context.lookup("CustomerOrderManager");
+      final Address address = new Address();
+      address.setCity("San Mateo");
+      address.setState("CA");
+      address.setZipCode(94402L);
+      final Customer customer = new Customer();
+      customer.setEmail("wayzout@gmail.com");
+      customer.setShippingAddress(address);
+      custOrderMgr.persistEntity(customer);
+
+      for (Customer cust: 
+           custOrderMgr.queryCustomerFindByEmail("wayzout@gmail.com")) {
+        System.out.println("Customer ID is " + cust.getId());
+      }
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
